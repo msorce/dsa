225f1252ab11f57c1f7327b79be30f96205afbdb
msorce <msorce@wsgc.com>
2019-10-11 08:09:45 -0700

    ranges sum solution js

--- algorithms/javascript/ranges-sum.js
+++ algorithms/javascript/ranges-sum.js
@@ -7,10 +7,29 @@ if the end value of previous range is larger than current range
 use the previous range end for the current range start
 */
 
-const ranges = [ [5,15], [12,20], [25,45] ];
+const ranges = [
+    [5, 15],
+    [12, 20],
+    [25, 45]
+];
 
-function sumOverlappingRanges( ranges ) {
-    
+function merge(intervals) {
+
+    if (!intervals.length) {
+        return intervals;
+    }
+    intervals.sort((a, b) => a[0] - b[0]);
+    let prev = intervals[0];
+    let res = [prev];
+    for (let curr of intervals) {
+        if (curr[0] <= prev[1]) {
+            prev[1] = Math.max(prev[1], curr[1]);
+        } else {
+            res.push(curr);
+            prev = curr;
+        }
+    }
+    return res;
 }
 
-console.log(sumOverlappingRanges(ranges));
+console.log(merge(ranges))
